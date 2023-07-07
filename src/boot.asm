8d9728a5cefb75ab6e7d8ab80656c4370b8b8bb4
zgscsed <1106659535@qq.com>
2023-07-07 20:02:37 +0800

    实模式打印

--- src/boot.asm
+++ src/boot.asm
@@ -11,14 +11,29 @@ mov es, ax
 mov ss, ax
 mov sp, 0x7c00
 
-; 0xb8000  文本显示器的内存区域
-mov ax, 0xb800
-mov ds, ax
-mov byte [0], 'H'
+; bochs 魔术断点
+xchg bx, bx
+
+mov si, booting
+call print
 
 ; 阻塞
 jmp $
 
+print:
+    mov ah, 0x0e
+.next:
+    mov al, [si]
+    cmp al, 0
+    jz .done
+    int 0x10
+    inc si
+    jmp .next
+.done:
+    ret
+
+booting:
+    db "Booting znix...", 10, 13, 0;    \n\r
 
 ; 填充 0
 times 510 - ($ - $$) db 0
