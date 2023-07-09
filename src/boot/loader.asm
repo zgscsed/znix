d3c5d1d5608963f3843f49fcd825d10bb2d9808a
zgscsed <1106659535@qq.com>
2023-07-09 21:39:47 +0800

    进入内核

--- src/boot/loader.asm
+++ src/boot/loader.asm
@@ -39,11 +39,12 @@ detect_memory:
 
     mov si, detecting
     call print
+    ; xchg bx, bx
 
+    ; mov byte [0xb8000], 'P'
     jmp prepare_protected_mode
 
 prepare_protected_mode:
-    xchg bx, bx
 
     cli; 关闭中断
 
@@ -52,15 +53,14 @@ prepare_protected_mode:
     or al, 0b10
     out 0x92, al
 
-    ; 加载gdt
-    lgdt[gdt_ptr]; 加载 gdt
+    lgdt [gdt_ptr]; 加载 gdt
 
     ; 启动保护模式
     mov eax, cr0
     or eax, 1
     mov cr0, eax
 
-    ; 用跳转来刷新缓存
+    ; 用跳转来刷新缓存，启用保护模式
     jmp dword code_selector:protect_mode
 
 ; 阻塞
@@ -92,20 +92,99 @@ error:
 
 [bits 32]
 protect_mode:
-    xchg bx, bx
     mov ax, data_selector
+    mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
-    mov ss, ax;  初始化段寄存器
+    mov ss, ax; 初始化段寄存器
 
     mov esp, 0x10000; 修改栈顶
 
-    mov byte [0xb8000], 'p'
+    ; mov byte [0xb8000], 'p'
 
-    mov byte [0x200000], 'p'
+    ; mov byte [0x200000], 'p'
 
-jmp $
+    mov edi, 0x10000;  读取的目标内存
+    mov ecx, 10; 起始扇区
+    mov bl, 200; 扇区数量
+
+    call read_disk
+
+    jmp dword code_selector:0x10000
+
+    ud2; 表示出错
+
+
+read_disk:
+
+    ; 设置读写扇区的数量
+    mov dx, 0x1f2
+    mov al, bl
+    out dx, al
+
+    inc dx; 0x1f3
+    mov al, cl; 起始扇区的前八位
+    out dx, al
+
+    inc dx; 0x1f4
+    shr ecx, 8
+    mov al, cl; 起始扇区的中八位
+    out dx, al
+
+    inc dx; 0x1f5
+    shr ecx, 8
+    mov al, cl; 起始扇区的高八位
+    out dx, al
+
+    inc dx; 0x1f6
+    shr ecx, 8
+    and cl, 0b1111; 将高四位置为 0
+
+    mov al, 0b1110_0000;
+    or al, cl
+    out dx, al; 主盘 - LBA 模式
+
+    inc dx; 0x1f7
+    mov al, 0x20; 读硬盘
+    out dx, al
+
+    xor ecx, ecx; 将 ecx 清空
+    mov cl, bl; 得到读写扇区的数量
+
+    .read:
+        push cx; 保存 cx
+        call .waits; 等待数据准备完毕
+        call .reads; 读取一个扇区
+        pop cx; 恢复 cx
+        loop .read
+
+    ret
+
+    .waits:
+        mov dx, 0x1f7
+        .check:
+            in al, dx
+            jmp $+2; nop 直接跳转到下一行
+            jmp $+2; 一点点延迟
+            jmp $+2
+            and al, 0b1000_1000
+            cmp al, 0b0000_1000
+            jnz .check
+        ret
+
+    .reads:
+        mov dx, 0x1f0
+        mov cx, 256; 一个扇区 256 字
+        .readw:
+            in ax, dx
+            jmp $+2; 一点点延迟
+            jmp $+2
+            jmp $+2
+            mov [edi], ax
+            add edi, 2
+            loop .readw
+        ret
 
 code_selector equ (1 << 3)
 data_selector equ (2 << 3)
