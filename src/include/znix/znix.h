768f00199e4528b73f9df2d683dba615f8eaf86a
zgscsed <1106659535@qq.com>
2023-07-11 20:34:47 +0800

    编译和链接

---
+++ src/include/znix/znix.h
@@ -0,0 +1,8 @@
+#ifndef ZNIX_H_
+#define ZNIX_H_
+
+#define ZNIX_MAGIC 20230711
+
+void kernel_init();   // 初始化内核
+
+#endif
---
+++ src/kernel/main.c
@@ -0,0 +1,14 @@
+#include <znix/znix.h>
+
+int magic = ZNIX_MAGIC;
+
+char message[] = "hello znix!!!";       // .data
+char buf[1024];                        //  .bss
+void kernel_init()
+{
+    char *video = (char*)0xb8000;  // 文本显示器的内存位置
+    for (int i = 0; i < sizeof(message); ++i)
+    {
+        video[i * 2] = message[i];
+    }
+}
