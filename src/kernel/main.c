65b8d8e274b36105f3875fbb0b5459d22b1f3eb2
zgscsed <1106659535@qq.com>
2023-09-25 22:27:04 +0800

    中断控制器

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -16,10 +16,12 @@ void kernel_init()
     interrupt_init();
     // task_init();
 
-    asm volatile(
-        "sti\n"
-        "movl %eax, %eax\n"
-    );
+    // asm volatile(
+    //     "sti\n"
+    //     "movl %eax, %eax\n"
+    // );
+
+    asm volatile("sti\n");
 
     u32 counter = 0;
     while (true)
